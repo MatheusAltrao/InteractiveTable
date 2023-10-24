99f63012e6ff19c0d4664be3ec2e8b0cfc361f51
Matheus Altrão <matheusaltrao2@gmail.com>
2023-10-23 22:11:09 -0400

    feat: create recent filter and ancient filter with select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,82 +10,91 @@ import {
 } from "@phosphor-icons/react";
 import { CaretLeft } from "@phosphor-icons/react/dist/ssr/CaretLeft";
 import { CaretRight } from "@phosphor-icons/react/dist/ssr/CaretRight";
-import { WarningCircle } from "@phosphor-icons/react/dist/ssr/WarningCircle";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { useEffect, useState } from "react";
+import { format } from "date-fns";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Home() {
   const [products, setProducts] = useState([
     {
       id: 1,
-      name: "Product 1",
-      color: "Red",
+      name: "Luxury Red Electronics",
+
       category: "Electronics",
       price: "$99.99",
       status: "disable",
       checked: false,
+      date: "2023-04-15",
     },
     {
       id: 2,
-      name: "Product 2",
-      color: "Blue",
+      name: "Stylish Blue Clothing",
+
       category: "Clothing",
       price: "$49.99",
       status: "sent",
       checked: false,
+      date: "2023-06-22",
     },
     {
       id: 3,
-      name: "Product 3",
-      color: "Green",
+      name: "Natural Green Home Decor",
+
       category: "Home Decor",
       price: "$29.99",
       action: "View Details",
       status: "active",
       checked: false,
+      date: "2023-01-10",
     },
     {
       id: 4,
-      name: "Product 4",
-      color: "Black",
+      name: "Premium Black Furniture",
+
       category: "Furniture",
       price: "$199.99",
       action: "Add to Wishlist",
       status: "sent",
       checked: false,
+      date: "2023-09-28",
     },
     {
       id: 5,
-      name: "Product 5",
-      color: "Yellow",
+      name: "Bright Yellow Beauty Products",
+
       category: "Beauty",
       price: "$14.99",
       status: "disable",
       checked: false,
+      date: "2023-07-03",
     },
     {
       id: 6,
-      name: "Product 6",
-      color: "White",
+      name: "Pure White Sports Equipment",
+
       category: "Sports",
       price: "$79.99",
       status: "active",
       checked: false,
+      date: "2023-02-18",
     },
     {
       id: 7,
-      name: "Product 7",
-      color: "Orange",
+      name: "Fun Orange Toys",
+
       category: "Toys",
       price: "$19.99",
       status: "disable",
       checked: false,
+      date: "2023-11-05",
     },
   ]);
+
   const [searchValue, setSeachValue] = useState("");
   const [isSending, setIsSending] = useState(false);
   const [countSelectedProducts, setCountSelectedProducts] = useState(0);
   const [allCheckeds, setAllCheckeds] = useState(false);
+  const [filter, setFilter] = useState("alphabetical");
 
   useEffect(() => {
     setCountSelectedProducts(products.filter((item) => item.checked).length);
@@ -109,9 +118,7 @@ export default function Home() {
           item.category
             .toLocaleLowerCase()
             .includes(searchValue.toLocaleLowerCase()) ||
-          item.color
-            .toLocaleLowerCase()
-            .includes(searchValue.toLocaleLowerCase()) ||
+          item.date.includes(searchValue.toLocaleLowerCase()) ||
           item.price
             .toLocaleLowerCase()
             .includes(searchValue.toLocaleLowerCase()) ||
@@ -190,14 +197,37 @@ export default function Home() {
     setProducts(updatedProducts);
   }
 
-  console.log(products);
+  const calculeteFilter = useMemo(() => {
+    switch (filter) {
+      case "alphabetical":
+        return filtred.sort((a, b) => (a.name > b.name ? 1 : -1));
+
+      case "priceUp":
+        return filtred.sort((a, b) => (a.price > b.price ? -1 : 1));
+
+      case "pricedown":
+        return filtred.sort((a, b) => (a.price > b.price ? 1 : -1));
+
+      case "status":
+        return filtred.sort((a, b) => (a.status > b.status ? 1 : -1));
+
+      case "ancient":
+        return filtred.sort((a, b) => (a.date > b.date ? 1 : -1));
+
+      case "recent":
+        return filtred.sort((a, b) => (a.date > b.date ? -1 : 1));
+
+      default:
+        return filtred;
+    }
+  }, [filter, filtred]);
+
+  console.log(filter);
 
   return (
-    <div className="bg-zinc-950 h-screen w-screen flex items-center justify-center flex-col gap-8">
-      <div>
-        <div
-          className={`flex items-center justify-between gap-8"
-          }`}>
+    <div className="bg-zinc-950 h-screen w-screen flex items-center pt-[150px] flex-col gap-8">
+      <div className="flex items-center flex-col justify-center">
+        <header className={`flex items-center  justify-between gap-8 w-full`}>
           <div
             className={`flex items-center gap-2 transition-opacity ${
               isSending ? "opacity-100" : "opacity-0 pointer-events-none"
@@ -219,7 +249,7 @@ export default function Home() {
             <p className="text-zinc-300">{countSelectedProducts} checkeds</p>
           </div>
 
-          <div className="flex items-center justify-center">
+          <div className="flex items-start justify-between w-full mb-2  ">
             <div className="relative w-[400px]">
               <MagnifyingGlass
                 className="text-zinc-400 absolute top-3 left-2"
@@ -229,7 +259,7 @@ export default function Home() {
                 value={searchValue}
                 onChange={(e) => setSeachValue(e.target.value)}
                 type="text"
-                className="h-10 rounded bg-zinc-800 mb-2 w-full  pl-8 text-zinc-200"
+                className="h-10 rounded bg-zinc-800  w-full  pl-8 text-zinc-200"
                 placeholder="Search for any item in the table"
               />
               {searchValue.length > 0 && (
@@ -241,6 +271,19 @@ export default function Home() {
                 </button>
               )}
             </div>
+
+            <select
+              id="filter"
+              defaultValue={filter}
+              onChange={(e) => setFilter(e.target.value)}
+              className="bg-zinc-800 w-[150px] h-10  text-zinc-50 text-sm rounded border-transparent border focus:ring-blue-500 focus:border-blue-500 p-2.5">
+              <option value="alphabetical">Alphabetical</option>
+              <option value="priceUp">Price Up</option>
+              <option value="pricedown">Price Down</option>
+              <option value="status">Status</option>
+              <option value="recent">Recent</option>
+              <option value="ancient">ancient</option>
+            </select>
           </div>
 
           <div
@@ -258,20 +301,20 @@ export default function Home() {
               Cancel
             </button>
           </div>
-        </div>
+        </header>
 
-        <div className="relative overflow-x-auto  rounded flex-col mt-4 border-[#3f3f3f] overflow-hidden border mx-auto">
-          {products.length > 0 ? (
-            <table className="  w-[820px] ">
-              <thead className="text-xs text-zinc-300 uppercase bg-zinc-800  ">
+        <div className="overflow-hidden rounded">
+          {calculeteFilter.length > 0 ? (
+            <table className="  w-[920px] ">
+              <thead className="text-xs text-zinc-300 text-left uppercase bg-zinc-800  ">
                 <tr>
                   <th className="p-4"></th>
-                  <th className="px-6 py-3">Product name</th>
-                  <th className="px-6 py-3">Color</th>
-                  <th className="px-6 py-3">Category</th>
-                  <th className="px-6 py-3">Price</th>
-                  <th className="px-6 py-3">Status</th>
-                  <th className="px-6 py-3">Action</th>
+                  <th className="px-4 py-3">Product name</th>
+                  <th className="px-4 py-3">Category</th>
+                  <th className="px-4 py-3">Price</th>
+                  <th className="px-4 py-3">Date</th>
+                  <th className="px-4 py-3">Status</th>
+                  <th className="px-4 py-3">Action</th>
                 </tr>
               </thead>
 
@@ -279,7 +322,7 @@ export default function Home() {
                 {filtred.map((item) => (
                   <tr
                     key={item.id}
-                    className="bg-zinc-900 h-10 text-zinc-300 border-zinc-800 border-b hover:bg-zinc-950 transition-colors ">
+                    className="bg-zinc-900 h-10 text-left text-zinc-300 border-zinc-800 border-b hover:bg-zinc-950 transition-colors ">
                     <th className=" flex items-center justify-center mt-3">
                       {item.status == "active" && (
                         <input
@@ -296,10 +339,12 @@ export default function Home() {
                         />
                       )}
                     </th>
-                    <th className="text-center">{item.name}</th>
-                    <th className="text-center">{item.color}</th>
-                    <th className="text-center">{item.category}</th>
-                    <th className="text-center">{item.price}</th>
+                    <th className="px-4">{item.name}</th>
+                    <th className="px-4">{item.category}</th>
+                    <th className="px-4">{item.price}</th>
+                    <th className="px-4">
+                      {format(new Date(item.date), "dd/MM/yyyy")}
+                    </th>
                     <th className="text-center flex items-center justify-center h-full">
                       {item.status == "active" && (
                         <p className=" py-0.5 w-[80px] bg-green-500 text-green-950 font-semibold text-center rounded">
@@ -349,15 +394,6 @@ export default function Home() {
             <p className="text-zinc-200 text-lg text-center py-4">
               There are no products
             </p>
-          )}
-
-          {products.length > 0 && filtred.length == 0 && (
-            <div className="flex items-center justify-center absolute top-16 gap-2">
-              <WarningCircle className="text-red-500 " size={22} />
-              <p className="text-zinc-200 text-lg text-center ">
-                check if you typed it correctly
-              </p>
-            </div>
           )}
         </div>
       </div>
